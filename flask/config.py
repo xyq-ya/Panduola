0bbd3bfe5344701169bb4a2672ead6dc91336544
3343465091 <3343465091@qq.com>
2025-11-07 16:46:10 +0800

    日历部分更新2

--- flask/config.py
+++ flask/config.py
@@ -1,6 +1,6 @@
 # config.py
 DB_HOST = "localhost"
 DB_USER = "root"
-DB_PASSWORD = "Aa12332166@"  # 修改成你自己的密码
+DB_PASSWORD = "yang20050219"  # 修改成你自己的密码
 DB_NAME = "task_management_system"
 DB_CHARSET = "utf8mb4"
